336c7beb584f55869df09e48fb501fdac04a61ce
DevDiner <0xirwin@gmail.com>
2026-09-26 05:45:58 +0900

    feat(contracts): MaruMaruRegistrar skeleton (state, events, errors, empty fns)

---
+++ contracts/src/MaruMaruRegistrar.sol
@@ -0,0 +1,5 @@
+function mintPassport(string calldata label, address owner, uint256 humanNullifier, bool transferable)
+    external onlyIssuer returns (uint256 tokenId) { /* TODO */ }
+function writeMark(...) external onlyIssuer { /* TODO */ }
+function grantLender(...) external onlyIssuer { /* TODO */ }
+// ...etc, all signatures, no bodies
